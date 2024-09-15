fdde41cd6b9dda4702d8b7f4c6740a75658090ab
Elizeu Almeida <elizeudiasalmeida@hotmail.com>
2024-09-15 14:15:41 -0300

    adicionar metas abertas

--- index.js
+++ index.js
@@ -47,6 +47,36 @@ const listarMeta = async () => {
 
 }
 
+const metasRealizadas = async () => {
+  const realizadas = metas.filter((meta) => {
+    return meta.checked;
+  })
+
+  if (realizadas.length == 0) {
+    console.log('Não existe metas realizadas! :C')
+    return;
+  }
+  await select({
+    message: 'Metas realizadas',
+    choices: [...realizadas],
+  })
+}
+
+const metasAbertas = async () => {
+  const abertas = metas.filter((meta) => {
+    return meta.checked != true;
+  })
+
+  if (abertas.lengt == 0) {
+    console.log('Não existem metas abertas! :3');
+    return;
+  }
+  await select({
+    message: 'Metas Abertas',
+    choices: [...abertas]
+  });
+}
+
 const start = async () => {
 
   while (true) {
@@ -63,6 +93,14 @@ const start = async () => {
           value: 'listar'
         },
         {
+          name: 'Metas Realizadas',
+          value: 'realizadas'
+        },
+        {
+          name: 'Metas Abertas',
+          value: 'abertas',
+        },
+        {
           name: 'Sair',
           value: 'sair'
         }
@@ -73,10 +111,15 @@ const start = async () => {
     switch (opcao) {
       case 'cadastrar':
         await cadastrarMeta();
-        console.log(metas);
         break;
       case 'listar':
         await listarMeta();
+        break;
+      case 'realizadas':
+        await metasRealizadas();
+        break;
+      case 'abertas':
+        await metasAbertas();
         break;
       case 'sair':
         console.log('Até a próxima!');
